f6b4f40516bff2d64a5d0cf81bbf905072750303
gsrinu002 <gsrinu002@gmail.com>
2025-06-11 21:07:56 +0530

    Add files via upload

---
+++ WEEK-1/First.java
@@ -0,0 +1,11 @@
+public class First
+{
+    public static void main(String[] args) {
+
+        System.out.println("hello java");
+        int x=10;
+        int y=20;
+        int add=x+y;
+        System.out.println("addtion is.."+ add);
+    }
+}
